ad47b3ed66a0a750c864935ccbbcb6d2aeafada4
Julian Obrecht <julian.obrecht@hivesound.ai>
2024-05-27 11:12:42 +0200

    Stage 3 - install packages
    Add alloy and ngrok to image

---
+++ stage3/00-install-packages/00-run-chroot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# add apt repos
+## add grafana alloy
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+cat /etc/apt/sources.list.d/grafana.list
+
+## add ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| tee /etc/apt/sources.list.d/ngrok.list
+
+# update
+apt update
+
+# install
+apt install -y alloy ngrok
+systemctl enable alloy.service
