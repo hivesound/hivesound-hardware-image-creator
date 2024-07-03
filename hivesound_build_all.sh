05e78f654f76517de9a1ab13882150449638f54f
kevinkrs <kraus.kevin@proton.me>
2024-07-03 16:53:22 +0200

    added auto hivesound-gateway code cloning  or updating  before running build

--- hivesound_build_all.sh
+++ hivesound_build_all.sh
@@ -5,5 +5,16 @@ rm -f stage3/SKIP
 rm -f stage4/SKIP
 rm -f stage5/SKIP
 
+if [ -d "stage4/00-install-gateway-app/app/.git" ]; then
+  echo "Directory contains a Git repository. Pulling the latest version..."
+  git -C "stage4/00-install-gateway-app/app" fetch origin
+  git -C "stage4/00-install-gateway-app/app" reset --hard origin/hivesound
+else
+  echo "Directory does not contain a Git repository. Cloning the repository..."
+  rm -rf "stage4/00-install-gateway-app/app"
+  git clone "git@github.com:hivesound/hivesound-gateway.git" "stage4/00-install-gateway-app/app"
+  git -C "stage4/00-install-gateway-app/app" checkout hivesound
+fi
+
 docker rm -v pigen_work
 PRESERVE_CONTAINER=1 PIGEN_DOCKER_OPTS="--env-file build_config/build.env" ./build-docker.sh
--- hivesound_rebuild_app_and_configs_stage.sh
+++ hivesound_rebuild_app_and_configs_stage.sh
@@ -4,4 +4,15 @@ touch stage2/SKIP
 touch stage3/SKIP
 rm -f stage4/SKIP
 
+if [ -d "stage4/00-install-gateway-app/app/.git" ]; then
+  echo "Directory contains a Git repository. Pulling the latest version..."
+  git -C "stage4/00-install-gateway-app/app" fetch origin
+  git -C "stage4/00-install-gateway-app/app" reset --hard origin/hivesound
+else
+  echo "Directory does not contain a Git repository. Cloning the repository..."
+  rm -rf "stage4/00-install-gateway-app/app"
+  git clone "git@github.com:hivesound/hivesound-gateway.git" "stage4/00-install-gateway-app/app"
+  git -C "stage4/00-install-gateway-app/app" checkout hivesound
+fi
+
 PRESERVE_CONTAINER=1 CONTINUE=1 CLEAN=1 PIGEN_DOCKER_OPTS="--env-file build_config/build.env" ./build-docker.sh
