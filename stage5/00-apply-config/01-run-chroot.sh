8f23d2b2692c522d268eae6266294372b8b15205
Julian Obrecht <julian.obrecht@hivesound.ai>
2024-07-03 16:03:40 +0200

    remove ngrok service from autostart

--- stage5/00-apply-config/01-run-chroot.sh
+++ stage5/00-apply-config/01-run-chroot.sh
@@ -6,7 +6,7 @@ echo net.ipv6.conf.default.disable_ipv6=1 >> /etc/sysctl.conf
 echo net.ipv6.conf.lo.disable_ipv6=1 >> /etc/sysctl.conf
 
 # setup ngrok
-systemctl enable ngrok.service
+systemctl disable ngrok.service
 
 # activate cronjobs
 crontab /home/admin/crontab_config
