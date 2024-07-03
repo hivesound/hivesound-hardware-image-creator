45043afe7d18d4f8e489bfc6c593d9e7ee7f8509
Julian Obrecht <julian.obrecht@hivesound.ai>
2024-07-03 15:56:05 +0200

    Apply credentials from env to config files

--- stage5/00-apply-config/00-run.sh
+++ stage5/00-apply-config/00-run.sh
@@ -6,17 +6,33 @@ APP_HOME=${ROOTFS_DIR}/home/admin/IND0906BLE
 
 cd config
 cp -r hivesound $APP_HOME/
+
+# setup app config
 cp config.yml $APP_HOME/
+sed -i "s/\${AWS_CLIENT_ID}/$AWS_CLIENT_ID/g" $APP_HOME/config.yml
+
+# setup logging config
 cp logging.yml $APP_HOME/
+sed -i "s#\${OPEN_OBSERVE_LOGS_URL}#$OPEN_OBSERVE_LOGS_URL#g" $APP_HOME/logging.yml
+sed -i "s/\${OPEN_OBSERVE_LOGS_AUTH_HEADER_VALUE}/$OPEN_OBSERVE_LOGS_AUTH_HEADER_VALUE/g" $APP_HOME/logging.yml
+
+# setup credentials
+mkdir -p $APP_HOME/hivesound/aws
+echo -e $AWS_CLIENT_CERTIFICATE > $APP_HOME/hivesound/aws/client-certificate.pem
+echo -e $AWS_CLIENT_PRIVATE_KEY > $APP_HOME/hivesound/aws/client-private.key
 
 cp crontab_config $USER_HOME/
 
+# setup alloy
 mkdir -p ${ROOTFS_DIR}/etc/alloy
 cp config.alloy ${ROOTFS_DIR}/etc/alloy/config.alloy
+sed -i "s/\${ALLOY_AGENT_REMOTE_TARGET_USERNAME}/$ALLOY_AGENT_REMOTE_TARGET_USERNAME/g" ${ROOTFS_DIR}/etc/alloy/config.alloy
+sed -i "s/\${ALLOY_AGENT_REMOTE_TARGET_PASSWORD}/$ALLOY_AGENT_REMOTE_TARGET_PASSWORD/g" ${ROOTFS_DIR}/etc/alloy/config.alloy
+
 
 # ngrok
 mkdir -p ${ROOTFS_DIR}/home/admin/.config/ngrok
 cp ngrok.yml ${ROOTFS_DIR}/home/admin/.config/ngrok/ngrok.yml
 # replace placeholder with actual ngrok token of this build
-sed -i "s/\${NGROK_TOKEN}/${NGROK_TOKEN}/g" ${ROOTFS_DIR}/home/admin/.config/ngrok/ngrok.yml
+sed -i "s/\${NGROK_TOKEN}/$NGROK_TOKEN/g" ${ROOTFS_DIR}/home/admin/.config/ngrok/ngrok.yml
 cp ngrok.service ${ROOTFS_DIR}/etc/systemd/system/ngrok.service
