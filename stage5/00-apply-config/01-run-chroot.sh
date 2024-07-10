ccead78b13dacdaf7c70743bec1c4c187ce4688c
Julian Obrecht <julian.obrecht@hivesound.ai>
2024-07-10 21:11:04 +0200

    removed not required ipv6 restrictions

--- stage5/00-apply-config/01-run-chroot.sh
+++ stage5/00-apply-config/01-run-chroot.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-# fix ip resolving issue with ipv6 -> deactivate ipv6
-echo net.ipv6.conf.all.disable_ipv6=1 >> /etc/sysctl.conf
-echo net.ipv6.conf.default.disable_ipv6=1 >> /etc/sysctl.conf
-echo net.ipv6.conf.lo.disable_ipv6=1 >> /etc/sysctl.conf
-
 # setup ngrok
 systemctl disable ngrok.service
 systemctl enable activate_lte_apn.service
