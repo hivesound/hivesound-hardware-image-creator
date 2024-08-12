40f92caf77f2a0c20b00f868aa2c233f9620517f
Julian Obrecht <julian.obrecht@hivesound.ai>
2024-08-12 16:52:59 +0200

    Set correct serial settings for lte hat

--- stage5/00-apply-config/01-run-chroot.sh
+++ stage5/00-apply-config/01-run-chroot.sh
@@ -2,6 +2,14 @@
 
 # setup ngrok
 systemctl disable ngrok.service
+
+# setup lte hat
+## To use the hardware serial port, you need to first disable the login shell function and then enable the hardware serial.
+## https://www.waveshare.com/wiki/A7670E_Cat-1_HAT
+## for some reason 1 means off and 0 means on
+raspi-config nonint do_serial_cons 1
+raspi-config nonint do_serial_hw 0
+## activate lte hat service
 systemctl enable lte_hat.service
 
 # activate cronjobs
